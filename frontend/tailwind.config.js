b261bc633a304d29ab3d45ab56198c1a30da7eed
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-25 14:49:36 +0700

    config global style tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,50 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          "Oxygen-Sans",
+          "Ubuntu",
+          "Cantarell",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      fontSize: {
+        base: "14px",
+      },
+      lineHeight: {
+        snugger: "1.285",
+      },
+      height: {
+        16: "50px",
+      },
+      backgroundColor: {
+        "teal-900": "#03363d",
+        "gray-100": "#f8f9f9",
+      },
+      borderColor: {
+        "green-500": "#78a300",
+      },
+      width: {
+        64: "16rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      cursor: ["focus"],
+      width: ["focus"],
+    },
   },
   plugins: [],
-}
+};
