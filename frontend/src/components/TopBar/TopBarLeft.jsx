51b453420a8f90359e8110ae26019a92fad22df5
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-25 14:45:30 +0700

    membuat top bar dashboard

---
+++ frontend/src/components/TopBar/TopBarLeft.jsx
@@ -0,0 +1,21 @@
+import AddDropdown from "./TopBarLeft/AddDropdown";
+
+const TopBarLeft = () => {
+    const menuItems = [
+        { label: 'Agency', href: '/agency' },
+        { label: 'Category', href: '/category' },
+    ];
+    return (
+        <ul
+            aria-label="top bar left"
+            aria-orientation="horizontal"
+            className="flex items-center"
+        >
+            <li className="group relative">
+                <AddDropdown items={menuItems} />
+            </li>
+        </ul>
+    );
+}
+
+export default TopBarLeft;
