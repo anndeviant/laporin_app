e9e598a80143581c5c5e53eac2eac70c232c4472
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-27 00:34:55 +0700

    modifikasi komponen, untuk multi function

--- frontend/src/components/Form/SelectField.jsx
+++ frontend/src/components/Form/SelectField.jsx
@@ -4,12 +4,12 @@ const SelectField = ({ id,
     label,
     className = "border border-sky-500 focus:outline-none focus:border-2 focus:border-sky-500 focus:shadow-[0_0_5px_rgba(0,191,255,10)] hover:shadow-[0_0_5px_rgba(0,191,255,10)] transition-shadow ease-in-out focus:ring-opacity-50",
     style = {},
-    sub=null,
+    sub = null,
     ...rest
 }) => {
     return (
         <div className="py-2">
-            {sub?(<p className="font-bold text-black py-2">{sub} :</p>) : null}
+            {sub ? (<p className="font-bold text-black py-2">{sub} :</p>) : null}
             <select
                 id={id}
                 name={name}
@@ -17,9 +17,15 @@ const SelectField = ({ id,
                 style={{ backgroundColor: "#ffffff", ...style }}
                 {...rest}
             >
-                {options.map((opt, i) => (
-                    <option key={i} value={opt.value || opt}>{opt.label || opt}</option>
-                ))}
+                {options.map((opt, i) => {
+                    const value = typeof opt === "object" ? opt.value : opt;
+                    const label = typeof opt === "object" ? opt.label : opt;
+                    return (
+                        <option key={i} value={value}>
+                            {label}
+                        </option>
+                    );
+                })}
             </select>
         </div>
     )
