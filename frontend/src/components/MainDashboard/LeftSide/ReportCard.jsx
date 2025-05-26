3a3d52666cc6c4ec970b805e4f941c92d72dd306
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-26 14:57:56 +0700

    menampilkan agency dan category

--- frontend/src/components/MainDashboard/LeftSide/ReportCard.jsx
+++ frontend/src/components/MainDashboard/LeftSide/ReportCard.jsx
@@ -9,7 +9,8 @@ const ReportCard = ({
   reporter_name,
   createdAt,
   location,
-  onUpdate, // opsional, untuk memberitahu parent ada update
+  government_agency,
+  report_category,
 }) => {
   const [loading, setLoading] = useState(false);
 
@@ -17,7 +18,6 @@ const ReportCard = ({
     setLoading(true);
     try {
       const res = await axios[method](`${BASE_URL}/admin/reports/${id}`, data);
-      const dataBaru = res.data;
     } catch (err) {
       console.error(err);
       alert("Gagal update laporan.");
@@ -39,23 +39,24 @@ const ReportCard = ({
       tabIndex="0"
       className="cursor-pointer border rounded-md p-3 bg-white flex flex-col text-gray-700 mb-2 hover:border-green-500 focus:outline-none focus:border-green-500"
     >
-        <header className="mb-1">
-          <span className="font-semibold">{reporter_name}</span> reported{" "}
-          <h1 className="inline">"{title}"</h1>
-        </header>
-        <p className="text-gray-600 text-sm line-clamp-2">{description}</p>
-        <div className="flex justify-between mt-2 text-sm text-gray-500">
-          
-          <span>{new Date(createdAt).toLocaleString()}</span>
-        </div>
-        <div className="text-xs mt-1 text-gray-400 italic">{location}</div>
-        <button
-          onClick={tanganiLaporan}
-          disabled={loading}
-          className="mt-3 self-end text-sm px-3 py-1 rounded bg-green-600 text-white hover:bg-green-700 disabled:opacity-50"
-        >
-          {loading ? "Memproses..." : "Terima Laporan"}
-        </button>
+      <header className="mb-1">
+        <span className="font-semibold">{reporter_name}</span> reported{" "}
+        <h1 className="inline">"{title}"</h1>
+      </header>
+      <p className="text-gray-600 text-sm line-clamp-2">{description}</p>
+      <span className="mt-1 text-xs italic text-gray-500">Agency: {government_agency?.name || "–"}</span>
+      <span className="mt-1 text-xs italic text-gray-500">Category: {report_category?.name || "–"}</span>
+      <div className="flex justify-between mt-2 text-sm text-gray-500">
+        <span>{new Date(createdAt).toLocaleString()}</span>
+      </div>
+      <div className="text-xs mt-1 text-gray-400 italic">{location}</div>
+      <button
+        onClick={tanganiLaporan}
+        disabled={loading}
+        className="mt-3 self-end text-sm px-3 py-1 rounded bg-green-600 text-white hover:bg-green-700 disabled:opacity-50"
+      >
+        {loading ? "Memproses..." : "Terima Laporan"}
+      </button>
     </article>
   );
 };
