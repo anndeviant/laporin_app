0c0133c0646ca5e62ec1b5d8347c4cc54cdc611f
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-05-27 12:49:53 +0700

    update routes fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
 import FormPage from "./pages/FormPage";
+import HelpPage from "./pages/HelpPage";
 import DashboardPage from "./pages/DashboardPage";
 import AgencyPage from "./pages/AgencyPage";
 import CategoryPage from "./pages/CategoryPage";
@@ -9,9 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin/" element={<LoginRegisterForm />} />
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/form" element={<FormPage />} />
+        <Route path="/bantuan" element={<HelpPage />} />
+        <Route path="/admin/" element={<LoginRegisterForm />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/agency" element={<AgencyPage />} />
         <Route path="/category" element={<CategoryPage />} />
       </Routes>
